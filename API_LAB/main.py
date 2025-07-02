fb926995154924fbc881a72eb9fc4166ab9d90c8
BELE104 <vinepesil@gmail.com>
2025-07-01 22:09:52 -0300

    v2

--- API_LAB/main.py
+++ API_LAB/main.py
@@ -7,6 +7,6 @@ def feth_data(endpoint):
     if data.status_code == 200:
         return data.json()
     else :
-        return print("LABA LABA DOOOOOO!!!!")
+        return print("LABA LABA DOOOOOO!!!!!!!!!!!!!!!!!!!!!!!")
 
 print(feth_data("episode"))
