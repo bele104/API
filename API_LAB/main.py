be2190aa664c24a9dbc80155110e5058a3bf02b7
BELE104 <vinepesil@gmail.com>
2025-07-13 17:14:13 -0300

    Banco

--- API_LAB/main.py
+++ API_LAB/main.py
@@ -1,14 +1,54 @@
 import requests
+import sqlite3
 
-
-def feth_data(topicos,filtros={}):
+def fetch_data(topicos,filtros={}):
 
     URL=f"https://rickandmortyapi.com/api/{topicos}"
-    data=requests.get(URL, params=filtros)
+   
+    try:   
+        resposta=requests.get(URL,params=filtros)
+        resposta.raise_for_status()
+        return resposta.json()
+    except requests.exceptions.HTTPError as erro_http:
+            print("Erro HTTP:", erro_http)
+            return None
+    except Exception as e:
+            print("Erro inesperado:", e)
+            return None
 
-    if data.status_code == 200:
-        return data.json()
-    else :
-        return print("LABA LABA DOOOOOO!!!!!!!!!!!!!!!!!!!!!!!")
+ 
+data=fetch_data("character",{'name':'Rick'})
+print("LABA LABA DOOOOOO!!!!!!!!!!!!!!!!!!!!!!!" )
+dataRick=sqlite3.connect("DataRick.db")
+controleData=dataRick.cursor()
+# Cria a tabela
+controleData.execute("""
+         CREATE TABLE IF NOT EXISTS ricks (
+        id INTEGER PRIMARY KEY,
+        nome TEXT,
+        status TEXT,
+        especie TEXT,
+        origem TEXT,
+        imagem TEXT
+    )
+""")
+if data:
+    for personagem in data["results"]:
+        id_ = personagem["id"]
+        nome = personagem["name"]
+        status = personagem["status"]
+        especie = personagem["species"]
+        origem = personagem["origin"]["name"]
+        imagem = personagem["image"]
+            
 
-print(feth_data("character",{'name':'Rick'}))
+        controleData.execute("""
+                INSERT OR IGNORE INTO ricks (id, nome, status, especie, origem, imagem)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (id_, nome, status, especie, origem, imagem))
+
+    dataRick.commit()
+    print("Ricks inseridos no banco com sucesso!")
+
+    # Fecha conexão
+    dataRick.close()
